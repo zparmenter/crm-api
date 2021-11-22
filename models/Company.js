f4d5927644eacc5a9ae1d2f0c00d799eb179136b
zparmenter <zparmenter20@gmail.com>
2021-11-22 12:24:36 -0600

    created first model and seeded data

---
+++ models/Company.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CompanySchema = new Schema({
+    companyName: {
+        type: String,
+        require: [true, 'Company Name must be provided.'],
+        unique: true,
+    },
+    companyContact: {
+        type: String,
+        require: [true, 'Contact must be provided.'],
+    },
+    email: {
+        type: String,
+        require: [true, 'Email must be provided.'],
+        unique: true,
+    },
+    phoneNumber: {
+        type: Number,
+        require: [true, 'Phone Number must be provided.'],
+    },
+    logo: {
+        type: String,
+        require: [true, 'Logo image must be provided.'],
+    },
+    images: [{
+        img: String,
+    }],
+});
+
+const Company = mongoose.model('Company', CompanySchema);
+
+module.exports = Company;
