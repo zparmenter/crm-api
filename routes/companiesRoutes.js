c9794876ae24a52f3de8d7529f6a9eb0ded6f5ba
zparmenter <zparmenter20@gmail.com>
2021-11-22 15:21:44 -0600

    full crud for company on server side

--- routes/companiesRoutes.js
+++ routes/companiesRoutes.js
@@ -5,6 +5,9 @@ const ctrl = require('../controllers');
 // routes
 router.get('/', ctrl.companies.index);
 router.get('/:id', ctrl.companies.show);
+router.post('/', ctrl.companies.create);
+router.put('/:id', ctrl.companies.update);
+router.delete('/:id', ctrl.companies.destroy);
 
 // exports
 module.exports = router;
